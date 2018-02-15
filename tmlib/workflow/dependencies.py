752f89e2ac5d5fc45d38a7a72f48c072960b8cd0
sparkvilla <diego.villamaina@gmail.com>
2018-02-15 15:48:53 +0000

    added missing curly brackets

--- tmlib/workflow/dependencies.py
+++ tmlib/workflow/dependencies.py
@@ -144,6 +144,7 @@ class CanonicalWorkflowDependencies(WorkflowDependencies):
         'popcon' : {
             'jterator'
     }
+}
 
 
 class MultiplexingWorkflowDependencies(CanonicalWorkflowDependencies):
